9c4a77d9f7d8505fc5757c1dade29f2d52c91883
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-07-06 21:56:19 +0200

    minor

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-System.register(['@angular/platform-browser-dynamic', './src/app/app.component'], function(exports_1, context_1) {
+System.register(['@angular/platform-browser-dynamic', 'src/app/app.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var platform_browser_dynamic_1, app_component_1;
@@ -15,4 +15,4 @@ System.register(['@angular/platform-browser-dynamic', './src/app/app.component']
         }
     }
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIm1haW4udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztZQU1BLG9DQUFTLENBQUMsNEJBQVksRUFBRSxFQUFFLENBQUMsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogQ3JlYXRlZCBieSB2YWRpbWRleiBvbiAyMS8wNi8xNi5cbiAqL1xuaW1wb3J0IHsgYm9vdHN0cmFwIH0gICAgZnJvbSAnQGFuZ3VsYXIvcGxhdGZvcm0tYnJvd3Nlci1keW5hbWljJztcbmltcG9ydCB7IEFwcENvbXBvbmVudCB9IGZyb20gJy4vc3JjL2FwcC9hcHAuY29tcG9uZW50JztcblxuYm9vdHN0cmFwKEFwcENvbXBvbmVudCwgW10pOyJdfQ==
+//# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@
  * Created by vadimdez on 21/06/16.
  */
 import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './src/app/app.component';
+import { AppComponent } from 'src/app/app.component';
 
 bootstrap(AppComponent, []);
