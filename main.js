4039de9ceb5f0a4e63ae676660a51e1a4a56fc77
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-07-06 22:07:13 +0200

    changed path back

--- main.js
+++ main.js
@@ -15,4 +15,4 @@ System.register(['@angular/platform-browser-dynamic', 'src/app/app.component'], 
         }
     }
 });
-//# sourceMappingURL=main.js.map
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIm1haW4udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztZQU1BLG9DQUFTLENBQUMsNEJBQVksRUFBRSxFQUFFLENBQUMsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogQ3JlYXRlZCBieSB2YWRpbWRleiBvbiAyMS8wNi8xNi5cbiAqL1xuaW1wb3J0IHsgYm9vdHN0cmFwIH0gICAgZnJvbSAnQGFuZ3VsYXIvcGxhdGZvcm0tYnJvd3Nlci1keW5hbWljJztcbmltcG9ydCB7IEFwcENvbXBvbmVudCB9IGZyb20gJ3NyYy9hcHAvYXBwLmNvbXBvbmVudCc7XG5cbmJvb3RzdHJhcChBcHBDb21wb25lbnQsIFtdKTsiXX0=
