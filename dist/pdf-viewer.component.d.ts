f01d9994c83e7fb50c86a3b035a222c9cd3cc41e
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-09-18 08:54:43 +0200

    Closes #7, updated dist

--- dist/pdf-viewer.component.d.ts
+++ dist/pdf-viewer.component.d.ts
@@ -3,19 +3,20 @@
  */
 import { ElementRef } from '@angular/core';
 export declare class PdfViewerComponent {
-    originalSize: boolean;
-    src: any;
-    page: number;
-    showAll: boolean;
-    private element;
-    private _src;
-    private _pdf;
-    private _page;
-    private _showAll;
-    constructor(element: ElementRef);
-    private fn();
-    private isValidPageNumber(page);
-    private renderPage(page);
-    private renderMultiplePages();
-    private isValidPageNumber();
+  originalSize: boolean;
+  src: any;
+  page: number;
+  showAll: boolean;
+  onLoadComplete: Function;
+  private element;
+  private _src;
+  private _pdf;
+  private _page;
+  private _showAll;
+  constructor(element: ElementRef);
+  private fn();
+  private isValidPageNumber(page);
+  private renderPage(page);
+  private renderMultiplePages();
+  private isValidPageNumber();
 }
--- dist/pdf-viewer.component.min.d.ts
+++ dist/pdf-viewer.component.min.d.ts
@@ -1,6 +1,22 @@
 /**
  * Created by vadimdez on 18/07/16.
  */
+import { ElementRef } from '@angular/core';
 export declare class PdfViewerComponent {
-
+  originalSize: boolean;
+  src: any;
+  page: number;
+  showAll: boolean;
+  onLoadComplete: Function;
+  private element;
+  private _src;
+  private _pdf;
+  private _page;
+  private _showAll;
+  constructor(element: ElementRef);
+  private fn();
+  private isValidPageNumber(page);
+  private renderPage(page);
+  private renderMultiplePages();
+  private isValidPageNumber();
 }
