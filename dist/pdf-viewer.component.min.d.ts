b7e6a01148a7c68a0fcf4ff28ac294f173f62890
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-09-26 12:23:17 +0200

    #5 but with RC4

--- dist/pdf-viewer.component.min.d.ts
+++ dist/pdf-viewer.component.min.d.ts
@@ -1,6 +1,21 @@
 /**
  * Created by vadimdez on 18/07/16.
  */
+import { ElementRef } from '@angular/core';
 export declare class PdfViewerComponent {
-
+  originalSize: boolean;
+  src: string;
+  page: number;
+  showAll: boolean;
+  private element;
+  private _src;
+  private _pdf;
+  private _page;
+  private _showAll;
+  constructor(element: ElementRef);
+  private fn();
+  private isValidPageNumber(page);
+  private renderPage(page);
+  private renderMultiplePages();
+  private isValidPageNumber();
 }
