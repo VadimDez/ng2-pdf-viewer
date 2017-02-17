5a57a967b485f59e73edccdd4d2e5a52e0a54ba0
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-02-17 12:14:47 +0100

    deleted dist folder from git

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var pdf_viewer_component_1 = require('./pdf-viewer.component');
-exports.PdfViewerComponent = pdf_viewer_component_1.PdfViewerComponent;
-//# sourceMappingURL=index.js.map
