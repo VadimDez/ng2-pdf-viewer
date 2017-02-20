718476b5fae2be2fc1cbd345b70fefa931a596ce
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-02-20 12:10:09 +0100

    Closes #54

--- build/app.module.js
+++ build/app.module.js
@@ -1,22 +1,27 @@
 "use strict";
-var core_1 = require('@angular/core');
-var forms_1 = require('@angular/forms');
-var platform_browser_1 = require('@angular/platform-browser');
-var app_component_1 = require('./app/app.component');
-var mdl_1 = require('./app/mdl');
-var index_1 = require('./pdf-viewer/index');
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var core_1 = require("@angular/core");
+var forms_1 = require("@angular/forms");
+var platform_browser_1 = require("@angular/platform-browser");
+var app_component_1 = require("./app/app.component");
+var mdl_1 = require("./app/mdl");
+var index_1 = require("./pdf-viewer/index");
 var AppModule = (function () {
     function AppModule() {
     }
-    AppModule.decorators = [
-        { type: core_1.NgModule, args: [{
-                    imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
-                    declarations: [mdl_1.MDL, app_component_1.AppComponent, index_1.PdfViewerComponent],
-                    bootstrap: [app_component_1.AppComponent]
-                },] },
-    ];
-    AppModule.ctorParameters = [];
     return AppModule;
 }());
+AppModule = __decorate([
+    core_1.NgModule({
+        imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
+        declarations: [mdl_1.MDL, app_component_1.AppComponent, index_1.PdfViewerComponent],
+        bootstrap: [app_component_1.AppComponent]
+    })
+], AppModule);
 exports.AppModule = AppModule;
 //# sourceMappingURL=app.module.js.map
