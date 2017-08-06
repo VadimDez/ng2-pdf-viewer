cc8419e490f690236ded4f83d3550b4711e2200e
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-08-06 09:58:32 +0200

    enabled prod mode

--- build/main.js
+++ build/main.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 var app_module_1 = require("./app.module");
+var core_1 = require("@angular/core");
+core_1.enableProdMode();
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,8 @@
  */
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
+import { enableProdMode } from '@angular/core';
+
+enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule);
