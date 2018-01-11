eab341344ac6530854a7f8c375f59d73cc6acd03
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2018-01-11 16:04:40 +0100

    #250 Replaced MDL with @angular/material

--- system.config.js
+++ system.config.js
@@ -7,6 +7,14 @@
     '@angular': './build/lib/@angular',
     'pdfjs-dist': './build/lib/pdfjs-dist',
     //'pdf-viewer': './dist/'
+
+    // material
+    '@angular/material': 'lib/@angular/material/bundles/material.umd.js',
+    '@angular/animations': 'lib/@angular/animations/bundles/animations.umd.min.js',
+    '@angular/animations/browser': 'lib/@angular/animations/bundles/animations-browser.umd.min.js',
+    '@angular/platform-browser/animations': 'lib/@angular/platform-browser/bundles/platform-browser-animations.umd.min.js',
+    '@angular/common/http': 'lib/@angular/common/bundles/common-http.umd.min.js',
+    'tslib': 'lib/tslib/tslib.js',
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
@@ -25,6 +33,25 @@
     'platform-browser-dynamic'
   ].forEach(function (pkgName) {
     packages['@angular/' + pkgName] = { main: 'bundles/' +  pkgName + '.umd.js', defaultExtension: 'js' };
+  });
+
+  [
+    'platform',
+    'a11y',
+    'bidi',
+    'coercion',
+    'keycodes',
+    'overlay',
+    'portal',
+    'collections',
+    'observers',
+    'accordion',
+    'scrolling',
+    'layout',
+    'table',
+    'stepper'
+  ].forEach(function (name) {
+    map['@angular/cdk/' + name] = 'lib/@angular/cdk/bundles/cdk-' + name + '.umd.js';
   });
 
   var config = {
