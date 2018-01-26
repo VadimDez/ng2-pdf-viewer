4212bf9ae0f742fa44fa513158465b98d38c3b47
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2018-01-26 23:45:08 +0100

    add loading indicator

--- build/pdf-viewer/index.js
+++ build/pdf-viewer/index.js
@@ -1,24 +1,18 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var pdf_viewer_component_1 = require("./pdf-viewer.component");
 var PdfViewerModule = (function () {
     function PdfViewerModule() {
     }
-    PdfViewerModule = __decorate([
-        core_1.NgModule({
-            declarations: [pdf_viewer_component_1.PdfViewerComponent],
-            exports: [pdf_viewer_component_1.PdfViewerComponent]
-        })
-    ], PdfViewerModule);
+    PdfViewerModule.decorators = [
+        { type: core_1.NgModule, args: [{
+                    declarations: [pdf_viewer_component_1.PdfViewerComponent],
+                    exports: [pdf_viewer_component_1.PdfViewerComponent]
+                },] },
+    ];
+    PdfViewerModule.ctorParameters = function () { return []; };
     return PdfViewerModule;
 }());
 exports.PdfViewerModule = PdfViewerModule;
-
 //# sourceMappingURL=index.js.map
