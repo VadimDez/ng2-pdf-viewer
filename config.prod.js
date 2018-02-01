80116aa03886d8d966b93ddc8174498dd0982379
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2018-02-01 22:37:08 +0100

    removed old files

--- config.prod.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Vadym Yatsyuk on 22/06/16
- */
-System.import('app.min.js')
-  .then(null, console.error.bind(console));
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by vadimdez on 21/06/16.
- */
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
