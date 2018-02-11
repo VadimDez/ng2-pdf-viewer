1cc711dbb7bed5985bf56128e7b95f3e44211f6c
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2018-02-11 19:53:23 +0100

    analytics

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+
+  document.write('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
+  document.write('<script>(adsbygoogle = window.adsbygoogle || []).push({ google_ad_client: "ca-pub-7000744132425449", enable_page_level_ads: true });</script>');
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
