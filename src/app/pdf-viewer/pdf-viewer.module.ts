66d40864df141d08e6e3dc40026d84644215c257
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2018-07-27 23:11:16 +0200

    minor

--- src/app/pdf-viewer/pdf-viewer.module.ts
+++ src/app/pdf-viewer/pdf-viewer.module.ts
@@ -4,7 +4,7 @@
 import { NgModule } from '@angular/core';
 
 import { PdfViewerComponent } from './pdf-viewer.component';
-import { PDFJSStatic } from "pdfjs-dist";
+import { PDFJSStatic } from 'pdfjs-dist';
 
 declare global {
   const PDFJS: PDFJSStatic;
