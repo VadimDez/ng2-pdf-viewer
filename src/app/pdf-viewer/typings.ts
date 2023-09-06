15cba0d603dcaec52f27cbdb89e97d3ef7cec0f6
shamoon <4887959+shamoon@users.noreply.github.com>
2023-09-06 16:42:38 -0700

    Update pdfjs-dist to 3.10.111

--- src/app/pdf-viewer/typings.ts
+++ src/app/pdf-viewer/typings.ts
@@ -2,7 +2,7 @@ export type PDFPageProxy = import('pdfjs-dist/types/src/display/api').PDFPagePro
 export type PDFSource = import('pdfjs-dist/types/src/display/api').DocumentInitParameters;
 export type PDFDocumentProxy = import('pdfjs-dist/types/src/display/api').PDFDocumentProxy;
 export type PDFDocumentLoadingTask = import('pdfjs-dist/types/src/display/api').PDFDocumentLoadingTask;
-export type PDFViewerOptions = import('pdfjs-dist/types/web/base_viewer').PDFViewerOptions;
+export type PDFViewerOptions = import('pdfjs-dist/types/web/pdf_viewer').PDFViewerOptions;
 
 export interface PDFProgressData {
   loaded: number;
